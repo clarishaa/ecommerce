5ddd1700ab30aa8ab918bd2ba64b1ff25c246850
clarishaa <jabonillo.clarish@minsu.edu.ph>
2023-09-25 23:05:49 +0800

    ...

---
+++ app/Views/include/end.php
@@ -0,0 +1,8 @@
+    <!-- jquery -->
+    <script src = "js/jquery-3.6.0.js"></script>
+    <!-- isotope js -->
+    <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
+    <!-- bootstrap js -->
+    <script src = "bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+    <!-- custom js -->
+    <script src = "js/script.js"></script>
